e0ca69a86d163b8d554c5bdfcff079d60f81090b
UHquokka <bananamayong@gmail.com>
2023-09-23 15:46:32 +0900

    ✨: 임시 Header 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import styled from "styled-components";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <Container>
+      <Header />
       <Outlet />
     </Container>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,17 @@
+import styled from "styled-components";
+import { FaRegHandPointLeft } from 'react-icons/fa';
+
 export default function Header() {
     return (
-        <div>
-            //backBtn
-        //title
-        // 
-        </div>
+        <HeaderWrapper>
+            <button><FaRegHandPointLeft /></button>
+            <h1>Disney Characters</h1>
+        </HeaderWrapper>
     );
 }
 
+const HeaderWrapper = styled.div`
+width: 100%;
+height: 5rem;
+background-color: blueviolet;
+`
