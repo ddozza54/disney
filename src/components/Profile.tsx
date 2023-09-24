1fc2a09a4918552f9777c6e8717e5ea64f206aa9
UHquokka <bananamayong@gmail.com>
2023-09-24 10:58:35 +0900

    🔨: Profile 컴포넌트 분리

---
+++ src/components/Profile.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import { Character } from '../types';
+
+export default function Profile({ id, imageUrl, name }: Character) {
+    const navigate = useNavigate();
+    return (
+        <ProfileWrapper>
+            <ProfileImgFrame onClick={() => { navigate(`character/${id}`) }}>
+                <ProfileImg src={imageUrl} />
+            </ProfileImgFrame>
+            <ProfileName>{name}</ProfileName>
+        </ProfileWrapper>
+    );
+}
+
+
+const ProfileWrapper = styled.div`
+    width: auto;
+    height: 10rem;
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-radius: 1rem;
+    background-color: rgba(255, 255, 255, 0.6);
+    box-shadow: 0px 0px 25px -4px rgba(255,255,255,0.75);
+-webkit-box-shadow: 0px 0px 25px -4px rgba(255,255,255,0.75);
+-moz-box-shadow: 0px 0px 25px -4px rgba(255,255,255,0.75);
+
+    &:hover{
+        cursor: pointer;
+        transform:scale(1.1);
+    background-color: rgba(255, 255, 255, 0.8);
+    transition: all 0.3s;
+    }
+`;
+
+const ProfileImgFrame = styled.div`
+    width: 8rem;
+    height: 8rem;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    overflow: hidden;
+`
+const ProfileImg = styled.img`
+width: 11rem;
+object-fit: cover;
+`
+const ProfileName = styled.h3`
+    margin-top: 0.5rem;
+`
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,11 @@ import { useQuery } from '@tanstack/react-query';
 import { fetchCharacters } from '../api';
 import styled from 'styled-components';
 import { Character } from '../types';
-import { useNavigate } from 'react-router-dom';
-
+import Profile from '../components/Profile';
 
 export default function Home() {
     const { isLoading, data } = useQuery(["allCharacters"], fetchCharacters)
-    const navigate = useNavigate();
-
+    console.log(data)
     return (
         <>
             {
@@ -18,12 +16,11 @@ export default function Home() {
                         <Chracters>
                             {data?.map((character: Character) => (
                                 <li key={character.id}>
-                                    <Profile>
-                                        <ProfileImgFrame onClick={() => { navigate(`character/${character.id}`) }}>
-                                            <ProfileImg src={character.imageUrl} />
-                                        </ProfileImgFrame>
-                                        <ProfileName>{character.name}</ProfileName>
-                                    </Profile>
+                                    <Profile
+                                        id={character.id}
+                                        imageUrl={character.imageUrl}
+                                        name={character.name}
+                                    />
                                 </li>
                             ))}
                         </Chracters>
@@ -36,48 +33,10 @@ export default function Home() {
 const Chracters = styled.div`
     width: 100%;
     height: 100vh;
-    margin-top: 4rem;
+    /* margin-top: 15rem; */
     display: grid;
     grid-column-gap: 0.5rem;
     grid-row-gap: 3rem;
     grid-template-columns: repeat(4, 1fr);
 grid-template-rows: auto;
-`
-
-const Profile = styled.div`
-    width: auto;
-    height: 10rem;
-    padding: 1rem;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    border-radius: 1rem;
-    background-color: rgba(255, 255, 255, 0.6);
-    box-shadow: 0px 0px 25px -4px rgba(255,255,255,0.75);
--webkit-box-shadow: 0px 0px 25px -4px rgba(255,255,255,0.75);
--moz-box-shadow: 0px 0px 25px -4px rgba(255,255,255,0.75);
-
-    &:hover{
-        cursor: pointer;
-        transform:scale(1.1);
-    background-color: rgba(255, 255, 255, 0.8);
-    transition: all 0.3s;
-    }
-`;
-
-const ProfileImgFrame = styled.div`
-    width: 8rem;
-    height: 8rem;
-    border-radius: 50%;
-    display: flex;
-    justify-content: center;
-    overflow: hidden;
-`
-const ProfileImg = styled.img`
-width: 11rem;
-object-fit: cover;
-`
-const ProfileName = styled.h3`
-    margin-top: 0.5rem;
 `
