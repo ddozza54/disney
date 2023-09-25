c09aab55a19559571150cb1b5e44a1324e92ba98
UHquokka <bananamayong@gmail.com>
2023-09-25 17:10:13 +0900

    ✨: Home page 에 loaderbar 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,16 +3,30 @@ import { fetchCharacters } from '../api';
 import styled from 'styled-components';
 import { Character } from '../types';
 import Profile from '../components/Profile';
+import LoadingBar from 'react-top-loading-bar';
+import { useEffect, useState } from 'react';
+
 
 export default function Home() {
     const { isLoading, data } = useQuery(["allCharacters"], fetchCharacters)
+    const [progress, setProgress] = useState(10);
+    useEffect(() => {
+        isLoading ? setProgress(progress + 10) : setProgress(progress + 90)
+    }, [isLoading])
     return (
         <>
+
             {
                 isLoading ?
-                    <span>Loading...</span> :
+                    (<div>
+                        <LoadingBar color="blue" progress={progress}
+                            onLoaderFinished={() => setProgress(0)} />
+                        <span>Loading...</span>
+                    </div>) :
                     (<UI>
                         <Chracters>
+                            <LoadingBar color="blue" progress={progress}
+                                onLoaderFinished={() => setProgress(100)} />
                             {data?.map((character: Character) => (
                                 <li key={character.id}>
                                     <Profile
